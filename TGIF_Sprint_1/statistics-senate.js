89001313e8326701c1d9b09ff733d485881f4f6f
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-01-15 13:23:10 +0100

    commit

--- TGIF_Sprint_1/statistics-senate.js
+++ TGIF_Sprint_1/statistics-senate.js
@@ -2,9 +2,24 @@
 
 /*********************************object*****************************************************/
 var statistics = {
-  countD: 0,
-  countR: 0,
-  countI: 0
+  // countD: 0,
+  // countR: 0,
+  // countI: 0,
+  democrats: {
+    count: 0,
+    percentage: 0,
+    party: "D"
+  },
+  republicans: {
+    count: 0,
+    percentage: 0,
+    party: "R"
+  },
+  independents: {
+    count: 0,
+    percentage: 0,
+    party: "D"
+  }
 };
 
 /*************************************first column************************/
@@ -13,41 +28,83 @@ var members = data.results[0].members;
 
 var tbody = document.getElementById("senate-data");
 
-calcStat();
+calcTotalMembers();
 
-function calcStat() {
+function calcTotalMembers() {
   for (var i = 0; i < members.length; i++) {
     var party = members[i].party;
 
     if (party === "D") {
       console.log(party);
-      statistics.countD++;
+      statistics.democrats.count++;
     }
 
     if (party === "R") {
       console.log(party);
-      statistics.countR++;
+      statistics.republicans.count++;
     }
     if (party === "I") {
       console.log(party);
-      statistics.countI++;
+      statistics.independents.count++;
     }
   }
+  console.log("democrats " + statistics.democrats.count);
 }
 
-//Render the first column, function calcStat()//
+// function to calculate total percentage votes
+
+////////////////// function calculateTotalPercentageVotes/////////////
+var membersRepArray = [];
+var membersDemArray = [];
+var membersIndArray = [];
+var total = 0;
+var averagePercentageRepublicans = total / statistics.republicans.count;
+var averagePercentageDemocrats = total / statistics.democrats.count;
+var averagePercentageIndependents = total / statistics.independents.count;
+
+function calculateTotalPercentageVotes() {
+  for (i = 0; i < members.length; i++) {
+    var party = members[i].party;
+    if (party === "R") {
+      membersRep = members[i].votes_with_party_pct;
+      total = total + membersRep;
+      membersRepArray.push(membersRep);
+      console.log(averagePercentageRepublicans);
+    } else if (party === "D") {
+      membersDem = members[i].votes_with_party_pct;
+      console.log(membersDem);
+      membersDemArray.push(membersDem);
+    } else if (party === "I") {
+      membersInd = members[i].votes_with_party_pct;
+      console.log(membersInd);
+      membersIndArray.push(membersInd);
+    }
+  }
+
+  console.log(
+    "averagepercentageRepublicans " + total / statistics.republicans.count
+  );
+}
+document.write(calculateTotalPercentageVotes(averagePercentageDemocrats));
+
+/////////////////////////Render table///////////////////////////
 
 function renderSenateAtGlance(target) {
   var tr = document.createElement("tr");
   var td1 = document.createElement("td");
   var td2 = document.createElement("td");
+  var td3 = document.createElement("td");
   td1.innerHTML = target;
+
   if (target === "Democrats") {
-    td2.innerHTML = statistics.countD;
+    td2.innerHTML = statistics.democrats.count;
+    td3.innerHTML = averagePercentageDemocrats;
   } else if (target === "Republicans") {
-    td2.innerHTML = statistics.countR;
+    td2.innerHTML = statistics.republicans.count;
+    td3.innerHTML = averagePercentageRepublicans;
   } else {
-    td2.innerHTML = statistics.countI;
+    td2.innerHTML = statistics.independents.count;
+    td3.innerHTML = averagePercentageIndependents;
   }
 
   tr.appendChild(td1);
@@ -71,32 +128,6 @@ renderSenateAtGlance("Independents");
 // tr.appendChild(td8);
 
 // tbody.appendChild(tr);
-
-/*************************************** Votes with party*************************************/
-
-var membersRepArray = [];
-var membersDemArray = [];
-var membersIndArray = [];
-
-function listsenate() {
-  for (i = 0; i < members.length; i++) {
-    var party = members[i].party;
-    if (party === "R") {
-      membersRep = members[i].votes_with_party_pct;
-      console.log(membersRep);
-      membersRepArray.push(membersRep);
-    } else if (party === "D") {
-      membersDem = members[i].votes_with_party_pct;
-      console.log(membersDem);
-      membersDemArray.push(membersDem);
-    } else if (party === "I") {
-      membersInd = members[i].votes_with_party_pct;
-      console.log(membersInd);
-      membersIndArray.push(membersInd);
-    }
-  }
-}
-document.write(listsenate());
 
 /**********************************************Least Engaged TABLE************************/
 
